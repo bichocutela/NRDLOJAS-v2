32da3aec06dce92a9c8f24bbf90034a686007ffc
bicho cutela <haydendanex@gmail.com>
2026-09-12 02:54:55 -0300

    feat(nrd): persistir vínculos de categorias

--- app/src/main/java/com/example/data/AppDatabase.kt
+++ app/src/main/java/com/example/data/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Product::class, DynamicTab::class], version = 5, exportSchema = false)
+@Database(entities = [Product::class, DynamicTab::class], version = 6, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
     abstract fun dynamicTabDao(): DynamicTabDao
